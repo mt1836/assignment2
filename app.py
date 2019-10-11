dcee77d3c46b7bde5787295f53b2415aac5fb8de
Michael Tse <mt1836@nyu.edu>
2019-10-11 17:15:57 -0400

    basic functionality no sessions

--- app.py
+++ app.py
@@ -1,65 +1,62 @@
+import subprocess
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from forms import RegistrationForm, LoginForm, SpellCheckForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
+@app.route("/results")
+def results():
+        global input_text
+        global spellcheck_results
+        return render_template('results.html')
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-     #   store in dictionary here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        flash(f'Success. Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        global userinfo
+        userinfo = {form.username.data:{'username':form.username.data, 'password':form.password.data, 'phone_number':form.phone_number.data}}
+        flash(f'Success!  Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    global userinfo
+    global authenticated
     if form.validate_on_submit():
-        if form.twofactor.data == '1234567890' and form.password.data == 'password':
+        if form.phone_number.data == userinfo[form.username.data]['phone_number'] and form.password.data == userinfo[form.username.data]['password']:
             flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('spellcheck'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login failure. Please check username, password or 2fa', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
 @app.route("/spellcheck", methods=['GET', 'POST'])
 def spellcheck():
     form = SpellCheckForm()
+    global input_text
+    global spellcheck_results
     if form.validate_on_submit():
-        input_text=form.checktext.data
-        flash(f'Text successfully submitted\n {input_text}', 'success')
-        return redirect(url_for('home'))
+        input_text = form.checktext.data
+        input_file = open("spellcheckfile.txt","w")
+        input_file.write(input_text)
+        input_file.close()
+        spellcheck_results = subprocess.check_output(["./a.out", "spellcheckfile.txt", "wordlist.txt"])
+        spellcheck_results=spellcheck_results.decode('utf-8')
+        spellcheck_results.replace("s",", ")
+        flash(f'Text below successfully submitted:', 'success')
+        flash(f'"{input_text}"', 'success')
+        return render_template('results.html', title='Spell Checker Results', spellcheck_results=spellcheck_results)
     return render_template('spellcheck.html', title='Spell Checker', form=form)
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,20 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=1, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=7,max=10)])
+    phone_number = StringField('Phone Number', validators= [Length(max=11)])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
-    twofactor = PasswordField('2fa', validators=[DataRequired()])
+    phone_number = StringField('2fa', validators= [Length(max=11)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
