87c50a364cace4a5e0719e59e31b97a4000a64d4
Michael Tse <mt1836@nyu.edu>
2019-10-11 20:22:27 -0400

    missing commas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import subprocess
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, session, g, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from forms import RegistrationForm, LoginForm, SpellCheckForm
@@ -11,9 +11,9 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 @app.route("/")
 @app.route("/results")
 def results():
-        global input_text
-        global spellcheck_results
-        return render_template('results.html')
+    global input_text
+    global spellcheck_results
+    return render_template('results.html')
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -31,9 +31,10 @@ def register():
 def login():
     form = LoginForm()
     global userinfo
-    global authenticated
+    session.pop('user', None)
     if form.validate_on_submit():
         if form.phone_number.data == userinfo[form.username.data]['phone_number'] and form.password.data == userinfo[form.username.data]['password']:
+            session['user'] = form.username.data
             flash('You have been logged in!', 'success')
             return redirect(url_for('spellcheck'))
         else:
@@ -41,23 +42,38 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+@app.route("/logout", methods=['GET'])
+def logout():
+    session.pop('user')
+    return redirect(url_for('login'))
+    
+
 @app.route("/spellcheck", methods=['GET', 'POST'])
 def spellcheck():
     form = SpellCheckForm()
     global input_text
     global spellcheck_results
-    if form.validate_on_submit():
-        input_text = form.checktext.data
-        input_file = open("spellcheckfile.txt","w")
-        input_file.write(input_text)
-        input_file.close()
-        spellcheck_results = subprocess.check_output(["./a.out", "spellcheckfile.txt", "wordlist.txt"])
-        spellcheck_results=spellcheck_results.decode('utf-8')
-        spellcheck_results.replace("s",", ")
-        flash(f'Text below successfully submitted:', 'success')
-        flash(f'"{input_text}"', 'success')
-        return render_template('results.html', title='Spell Checker Results', spellcheck_results=spellcheck_results)
-    return render_template('spellcheck.html', title='Spell Checker', form=form)
+    if g.user:
+        if form.validate_on_submit():
+            input_text = form.checktext.data
+            input_file = open("spellcheckfile.txt","w")
+            input_file.write(input_text)
+            input_file.close()
+            spellcheck_results = subprocess.check_output(["./a.out", "spellcheckfile.txt", "wordlist.txt"])
+            spellcheck_results=spellcheck_results.decode('utf-8')
+            spellcheck_results.replace("s",", ")
+            flash(f'Text below successfully submitted:', 'success')
+            flash(f'"{input_text}"', 'success')
+            return render_template('results.html', title='Spell Checker Results', spellcheck_results=spellcheck_results)
+        return render_template('spellcheck.html', title='Spell Checker', form=form)
+    return redirect(url_for('login'))
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
 if __name__ == '__main__':
     app.run(debug=True)
