67ee784d467eff6ece35378d6effd27415c3259d
Michael Tse <mt1836@nyu.edu>
2019-10-22 13:10:54 -0400

    Fixing Travis

--- test_apptest.py
+++ test_apptest.py
@@ -52,9 +52,8 @@ class TestSpellFunctions(unittest.TestCase):
         #print(spellpost.text)
         spellpostsoup = BeautifulSoup(spellpost.text, 'html.parser')
         spellpostinput = spellpostsoup.find_all('input')
-        spellpost_csrf_token = spellpostinput[0]['value']
+        spellpost_csrf_token = spellpostsoup.find('input', {'name': 'csrf_token'}).get('value') #spellpost_csrf_token = spellpostinput[0]['value']
         #print(spellpayload['csrf_token'])
-        #print(spellpost_csrf_token)
         spellpostinput = spellpostsoup.find_all('p')
         #print(spellpostinput)
         spell_message = spellpostinput[1].text
